2589e496d1b947d29215b29842068b2da91afe10
imsaiful <saifulcseng@gmail.com>
2018-11-12 13:27:13 +0530

    add template

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+
+
+def index(request):
+    message="Hello World"
+    return render(request,'feed/index.html',{"message":message})
