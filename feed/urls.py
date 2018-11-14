d0e1bfec8e1f3cac55756398509c7d694f885213
imsaiful <saifulcseng@gmail.com>
2018-11-15 03:01:52 +0530

    add news page

--- feed/urls.py
+++ feed/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-
+app_name = 'feed'
 
 urlpatterns = [
-    url(r'', views.index,name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^news$', views.news, name='news'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'feed/index.html', {})
+
+
+def news(request):
+    print("hello world")
+    message = "Pakistan can't even control its four provinces. It doesn't want Kashmir"
+    return render(request, 'feed/news.html', {"message": message})
