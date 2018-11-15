29064baef6a181dac8caa26c48fda295df7d9405
imsaiful <saifulcseng@gmail.com>
2018-11-16 00:16:03 +0530

    create first model

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Republic(models.Model):
+    Headline = models.TextField(null=False)
+    Link = models.TextField(null=False, primary_key=True)
+    Date = models.DateTimeField(default=timezone.now)
+    Category = models.TextField(default="uncategorized")
+    Sentiment = models.TextField(default="Positive")
+
+
+class Indiatoday(models.Model):
+    Headline = models.TextField(null=False)
+    Link = models.TextField(null=False, primary_key=True)
+    Date = models.DateTimeField(default=timezone.now)
+    Category = models.TextField(default="uncategorized")
+    Sentiment = models.TextField(default="Positive")
+
+
+class Ndtv(models.Model):
+    Headline = models.TextField(null=False)
+    Link = models.TextField(null=False, primary_key=True)
+    Date = models.DateTimeField(default=timezone.now)
+    Category = models.TextField(default="uncategorized")
+    Sentiment = models.TextField(default="Positive")
