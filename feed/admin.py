b2367f7b0c7163e1981a25d0a1e687ed5dd4acf1
imsaiful <saifulcseng@gmail.com>
2018-11-16 00:54:13 +0530

    design admin section

--- feed/admin.py
+++ feed/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Republic,Ndtv,Indiatoday
+
+
+admin.site.register(Republic)
+admin.site.register(Ndtv)
+admin.site.register(Indiatoday)
+
 
 # Register your models here.
--- feed/models.py
+++ feed/models.py
@@ -6,17 +6,29 @@ from django.utils import timezone
 class Republic(models.Model):
     Headline = models.TextField(null=False)
     Link = models.TextField(null=False, primary_key=True)
-    Date = models.DateTimeField(default=timezone.now)
-    Category = models.TextField(default="uncategorized")
-    Sentiment = models.TextField(default="Positive")
+    Date = models.DateTimeField(default=timezone.now, null=True)
+    Category = models.TextField(default="uncategorized", null=True)
+    Sentiment = models.TextField(default="Positive", null=True)
+
+    def __str__(self):
+        return self.Headline
+
+    class Meta:
+        ordering = ["-Date"]
 
 
 class Indiatoday(models.Model):
     Headline = models.TextField(null=False)
     Link = models.TextField(null=False, primary_key=True)
-    Date = models.DateTimeField(default=timezone.now)
-    Category = models.TextField(default="uncategorized")
-    Sentiment = models.TextField(default="Positive")
+    Date = models.DateTimeField(default=timezone.now, null=True)
+    Category = models.TextField(default="uncategorized", null=True)
+    Sentiment = models.TextField(default="Positive", null=True)
+
+    def __str__(self):
+        return self.Headline
+
+    class Meta:
+        ordering = ["-Date"]
 
 
 class Ndtv(models.Model):
@@ -25,3 +37,9 @@ class Ndtv(models.Model):
     Date = models.DateTimeField(default=timezone.now)
     Category = models.TextField(default="uncategorized")
     Sentiment = models.TextField(default="Positive")
+
+    def __str__(self):
+        return self.Headline
+
+    class Meta:
+        ordering = ["-Date"]
